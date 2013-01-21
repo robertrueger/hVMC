robertrueger/hVMC

commit 56e382cc80edd286ab3d8cd819a8102fcc52fe34

added a 2 bin queue to the flock followers code for a better overlap of communication and computation; the flock leader will now send out new work even if he is still equilibrating

hmodvmc.cpp
@@ -75,7 +75,6 @@ HubbardModelVMC::HubbardModelVMC(
     Wd_active(   M_init.ssym ? &Wd_1 : nullptr ),
     Wd_inactive( M_init.ssym ? &Wd_2 : nullptr ),
     T( Eigen::VectorXfp( lat->L ) ),
-    completed_mcsteps( 0 ),
     updates_until_W_recalc( updates_until_W_recalc_init ),
     updates_until_T_recalc( updates_until_T_recalc_init ),
     updates_since_W_recalc( 0 ), updates_since_T_recalc( 0 ),
@@ -232,17 +231,6 @@ void HubbardModelVMC::mcs()
 #endif
     metstep();
   }
-  ++completed_mcsteps;
-}
-
-
-
-void HubbardModelVMC::equilibrate( unsigned int N_mcs_equil )
-{
-  for ( unsigned int n = 0; n < N_mcs_equil; ++n ) {
-    mcs();
-  }
-  completed_mcsteps -= N_mcs_equil;
 }
 
 
@@ -679,13 +667,6 @@ fptype HubbardModelVMC::E_l()
 #endif
 
   return E_l_result;
-}
-
-
-
-unsigned long int HubbardModelVMC::mctime() const
-{
-  return completed_mcsteps;
 }
 
 

hmodvmc.hpp
@@ -83,8 +83,6 @@ class HubbardModelVMC final
     // (in order to avoid allocating new ones all the time)
     std::vector<unsigned int> k_pos_Xnn;
 
-    unsigned long int completed_mcsteps;
-
     // floating point precision control
     const unsigned int updates_until_W_recalc, updates_until_T_recalc;
     unsigned int updates_since_W_recalc, updates_since_T_recalc;
@@ -133,11 +131,9 @@ class HubbardModelVMC final
 
     // Monte Carlo step
     void mcs();
-    void equilibrate( unsigned int N_mcs_equil );
 
     // observable measurements
     fptype E_l();
-    unsigned long int mctime() const;
 
     // floating point precision control
     FPDevStat get_W_devstat() const;

simrun.cpp
@@ -24,9 +24,11 @@
 #include <chrono>
 #include <vector>
 #include <utility>
+#include <functional>
 
 #include <boost/optional.hpp>
 #include <boost/mpi/status.hpp>
+#include <boost/mpi/request.hpp>
 
 #include <eigen3/Eigen/Core>
 
@@ -53,14 +55,6 @@ BasicSimResults simrun_basic(
   HubbardModelVMC* model = nullptr;
   simrun_basic_prepare( opts, vpar, model, mpiflock );
 
-  if ( mpiflock.rank() == 0 ) {
-    cout << ":: Equilibrating the system" << endl;
-  }
-  model->equilibrate( opts["sim.num-mcs-equil"].as<unsigned int>() );
-
-  if ( mpiflock.rank() == 0 ) {
-    cout << ":: Performing Monte Carlo cycle" << endl;
-  }
   const BinnedData<fptype>& E_l = simrun_basic_mccycle( opts, model, mpiflock );
 
   // we don't need the simulation objects anymore ...
@@ -191,8 +185,50 @@ BinnedData<fptype> simrun_basic_mccycle(
     unsigned int completed_bins = 0;
     unsigned int enqueued_bins  = opts["sim.num-bins"].as<unsigned int>();
 
+    // define procedure to query the flock for new work requests
+    function<void()> mpiquery_work_requests( [&]() {
+      while ( boost::optional<mpi::status> status
+              = mpiflock.iprobe( mpi::any_source, 0 ) ) {
+        // receive the "I'm ready" message and hand out new bins to the source
+        mpiflock.recv( status->source(), 0 );
+        if ( enqueued_bins > 0 ) {
+          mpiflock.isend( status->source(), 1, 1 );
+          scheduled_bins += 1;
+          enqueued_bins  -= 1;
+        } else {
+          mpiflock.isend( status->source(), 1, 0 );
+          ++finished_flock_members;
+        }
+      }
+    } );
+
+    // define procedure to query the flock for finished work
+    function<void()> mpiquery_finished_work( [&]() {
+      while ( boost::optional<mpi::status> status
+                = mpiflock.iprobe( mpi::any_source, 2 ) ) {
+        mpiflock.recv( status->source(), 2 );
+        --scheduled_bins;
+        ++completed_bins;
+      }
+    } );
+
     unsigned int completed_bins_leader = 0;
 
+    cout << ":: Equilibrating the system" << endl;
+    for (
+      unsigned int mcs = 0;
+      mcs < opts["sim.num-mcs-equil"].as<unsigned int>();
+      ++mcs
+    ) {
+      // 1.: take care of the flock
+      mpiquery_finished_work();
+      mpiquery_work_requests();
+
+      // 2.: leader calculates a Monte Carlo step in his own cycle
+      model->mcs();
+    }
+
+    cout << ":: Performing Monte Carlo cycle" << endl;
     cout << endl;
     cout << "      Progress:" << endl;
 
@@ -207,35 +243,14 @@ BinnedData<fptype> simrun_basic_mccycle(
 
       E_l.append_empty_bins( 1 );
 
-      for ( unsigned int mcs = 0;
-            mcs < opts["sim.num-binmcs"].as<unsigned int>();
-            ++mcs ) {
-
-        // 1.: send new work to the flock
-
-        // check if anyone in the flock has finished his work
-        while ( boost::optional<mpi::status> status
-                = mpiflock.iprobe( mpi::any_source, 2 ) ) {
-          unsigned int completed_bins_fromsource;
-          mpiflock.recv( status->source(), 2, completed_bins_fromsource );
-          scheduled_bins -= completed_bins_fromsource;
-          completed_bins += completed_bins_fromsource;
-        }
-
-        // check if anyone in the flock is ready to accept new work
-        while ( boost::optional<mpi::status> status
-                = mpiflock.iprobe( mpi::any_source, 0 ) ) {
-          // receive the "I'm ready" message and hand out new bins to the source
-          mpiflock.recv( status->source(), 0 );
-          if ( enqueued_bins > 0 ) {
-            mpiflock.send( status->source(), 1, 1 );
-            scheduled_bins += 1;
-            enqueued_bins  -= 1;
-          } else {
-            mpiflock.send( status->source(), 1, 0 );
-            ++finished_flock_members;
-          }
-        }
+      for (
+        unsigned int mcs = 0;
+        mcs < opts["sim.num-binmcs"].as<unsigned int>();
+        ++mcs
+      ) {
+        // 1.: take care of the flock
+        mpiquery_finished_work();
+        mpiquery_work_requests();
 
         // 2.: leader calculates a Monte Carlo step in his own cycle
         model->mcs();
@@ -252,24 +267,22 @@ BinnedData<fptype> simrun_basic_mccycle(
             static_cast<int>( finished_flock_members ) < mpiflock.size() ) {
       if ( boost::optional<mpi::status> status
            = mpiflock.iprobe( mpi::any_source, 2 ) ) {
-        unsigned int completed_bins_fromsource;
-        mpiflock.recv( status->source(), 2, completed_bins_fromsource );
-        scheduled_bins -= completed_bins_fromsource;
-        completed_bins += completed_bins_fromsource;
-      }
-
-      if ( boost::optional<mpi::status> status
-           = mpiflock.iprobe( mpi::any_source, 0 ) ) {
-        // wait for the source to request more work
-        mpiflock.recv( status->source(), 0 );
-
-        // tell him there is no more work
-        mpiflock.send( status->source(), 1, 0 );
-        ++finished_flock_members;
+        mpiflock.recv( status->source(), 2 );
+        --scheduled_bins;
+        ++completed_bins;
 
         cout << '\r' << "        Bin "
              << completed_bins << "/" << opts["sim.num-bins"].as<unsigned int>();
         cout.flush();
+      }
+
+      if ( boost::optional<mpi::status> status
+           = mpiflock.iprobe( mpi::any_source, 0 ) ) {
+        // receive the request for more work
+        mpiflock.recv( status->source(), 0 );
+        // tell him there is no more work
+        mpiflock.isend( status->source(), 1, 0 );
+        ++finished_flock_members;
       }
     }
     assert( enqueued_bins == 0 );
@@ -308,37 +321,58 @@ BinnedData<fptype> simrun_basic_mccycle(
   } else {
     // ----- thread is following the flock -----
 
+   // equilibrate the system
+   for (
+      unsigned int mcs = 0;
+      mcs < opts["sim.num-mcs-equil"].as<unsigned int>();
+      ++mcs
+    ) {
+      model->mcs();
+    }
+
     unsigned int completed_bins_thisslave = 0;
+    bool leader_out_of_work = false;
+    unsigned int scheduled_bins_thisslave;
+    mpiflock.isend( 0, 0 );
+    mpiflock.recv( 0, 1, scheduled_bins_thisslave );
+    leader_out_of_work = ( scheduled_bins_thisslave == 0 );
 
-    while ( true ) {
+    while ( scheduled_bins_thisslave > 0 ) {
 
-      // ask the flock leader for new work
-      unsigned int scheduled_bins_thisslave;
-      mpiflock.send( 0, 0 );
-      mpiflock.recv( 0, 1, scheduled_bins_thisslave );
-
-      if ( scheduled_bins_thisslave > 0 ) {
-        // there is work to do, do it!
-
-        E_l.append_empty_bins( scheduled_bins_thisslave );
-
-        for ( unsigned int bin = 0; bin < scheduled_bins_thisslave; ++bin ) {
-          for ( unsigned int mcs = 0;
-                mcs < opts["sim.num-binmcs"].as<unsigned int>();
-                ++mcs ) {
-            model->mcs();
-            E_l[completed_bins_thisslave][mcs] = model->E_l();
-          }
-          ++completed_bins_thisslave;
-        }
-
-        // report completion of the work
-        mpiflock.send( 0, 2, scheduled_bins_thisslave );
-
-      } else {
-        // there is no more work, stop working!
-        break;
+      unsigned int new_scheduled_bins_thisslave;
+      mpi::request leader_answer;
+      if ( !leader_out_of_work ) {
+        // ask the flock leader for more work
+        mpiflock.isend( 0, 0 );
+        leader_answer = mpiflock.irecv( 0, 1, new_scheduled_bins_thisslave );
       }
+
+      E_l.append_empty_bins( 1 );
+
+      for (
+        unsigned int mcs = 0;
+        mcs < opts["sim.num-binmcs"].as<unsigned int>();
+        ++mcs
+      ) {
+        model->mcs();
+        E_l[completed_bins_thisslave][mcs] = model->E_l();
+      }
+
+      // report completion of the work
+      mpiflock.isend( 0, 2 );
+      ++completed_bins_thisslave;
+      --scheduled_bins_thisslave;
+
+      if ( !leader_out_of_work ) {
+        // wait for answer from leader concerning the next bin
+        leader_answer.wait();
+        if ( new_scheduled_bins_thisslave == 1 ) {
+          ++scheduled_bins_thisslave;
+        } else {
+          leader_out_of_work = true;
+        }
+      }
+
     }
 
   }

commit 01f64becc6048f95298f0dbf3c48907800467fbf

fixed a bug introduced in 56e382c that prevented debug builds from compiling

hmodvmc.cpp
@@ -226,8 +226,7 @@ void HubbardModelVMC::mcs()
   // perform a number of metropolis steps equal to the number of electrons
   for ( unsigned int s = 0; s < lat->L; ++s ) {
 #if VERBOSE >= 1
-    cout << "HubbardModelVMC::mcs() : Monte Carlo step = " << completed_mcsteps
-         << ", Metropolis step = " << s << endl;
+    cout << "HubbardModelVMC::mcs() : Metropolis step = " << s << endl;
 #endif
     metstep();
   }