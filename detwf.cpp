8dc016210723ef672c667e468b825f47a2c62405
Robert Rüger <rueger@itp.uni-frankfurt.de>
2013-04-14 15:16:47 +0200

    threw out the spin symmetry optimization to prepare for the implementation of the particle-hole transformation (will check later if I can make the two compatible, but for now lets just do it the easy way ...)

--- detwf.cpp
+++ detwf.cpp
@@ -34,50 +34,37 @@ SingleParticleOrbitals wf_tight_binding(
   // make sure we pass the right number of variational parameters
   assert( t.size() == 3  );
 
-  Eigen::MatrixXfp H_tb_nospin = Eigen::MatrixXfp::Zero( lat->L, lat->L );
+  Eigen::MatrixXfp H_tb = Eigen::MatrixXfp::Zero( 2*lat->L, 2*lat->L );
 
   vector<unsigned int> l_Xnn;
-  for ( unsigned int l = 0; l < lat->L; ++l ) {
+  for ( unsigned int l = 0; l < 2 * lat->L; ++l ) {
     for ( unsigned int X = 1; X <= 3; ++X ) {
       lat->get_Xnn( l, X, &l_Xnn );
       for ( auto it = l_Xnn.begin(); it != l_Xnn.end(); ++it ) {
-        H_tb_nospin( l, *it ) += -1.f * t[X - 1];
+        // TODO: reverse sign!!! (Robert Rueger, 2013-04-14 15:01)
+        H_tb( l, *it ) += ( l < lat->L ? -1.f : -1.f ) * t[X - 1];
       }
     }
   }
 
-#if VERBOSE >= 2
-  cout << "wf_tight_binding() : spinless Hamiltonian in single particle basis ="
-       << endl << H_tb_nospin << endl;
+#if VERBOSE >= 1
+  cout << "wf_tight_binding() : Hamiltonian in single particle basis ="
+       << endl << H_tb << endl;
 #endif
 
-  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXfp> eigensolver( H_tb_nospin );
+  Eigen::SelfAdjointEigenSolver<Eigen::MatrixXfp> eigensolver( H_tb );
   assert( eigensolver.info() == Eigen::Success );
 
   assert( N % 2 == 0 );
 
-/*
-  Eigen::MatrixXfp M = Eigen::MatrixXfp::Zero( 2 * lat->L, N );
-  for ( unsigned int k = 0; k < N / 2; ++k ) {
-    M.col( 2 * k ).head( lat->L )     = eigensolver.eigenvectors().col( k );
-    M.col( 2 * k + 1 ).tail( lat->L ) = eigensolver.eigenvectors().col( k );
-  }
-  Eigen::VectorXfp E = Eigen::VectorXfp::Zero( 2 * lat->L );
-  for ( unsigned int k = 0; k < lat->L; ++k ) {
-    E( 2 * k )     = eigensolver.eigenvalues()( k );
-    E( 2 * k + 1 ) = eigensolver.eigenvalues()( k );
-  }
-*/
-
   const Eigen::MatrixXfp& M
-    = eigensolver.eigenvectors().topLeftCorner( lat->L, N / 2 );
+    = eigensolver.eigenvectors().topLeftCorner( 2 * lat->L, N );
 
-#if VERBOSE >= 2
+#if VERBOSE >= 1
   cout << "wf_tight_binding() : M = " << endl << M << endl
        << "wf_tight_binding() : slater determinant ground state energy = "
-       << 2.f * eigensolver.eigenvalues().head( N / 2 ).sum()  << endl;
+       << eigensolver.eigenvalues().head( N ).sum()  << endl;
 #endif
 
-  //return SingleParticleOrbitals( M, E, false );
-  return SingleParticleOrbitals( M, eigensolver.eigenvalues(), true );
+  return SingleParticleOrbitals( M, eigensolver.eigenvalues(), false );
 }
